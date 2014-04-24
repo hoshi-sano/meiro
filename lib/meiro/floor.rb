2a3df9f76d3f36179a41e024a8f42accad7e555d
Yuki Morohoshi <hoshi.sanou@gmail.com>
2014-04-24 14:39:09 +0900

    誤ったコメントの修正。

--- lib/meiro/floor.rb
+++ lib/meiro/floor.rb
@@ -51,8 +51,8 @@ module Meiro
     end
 
     # 指定したx座標、y座標を含むBlockを返す。そのBlockが親(分割済)であっ
-    # た場合は、分割されたいずれかのうち、辺を共有している方を返す。該
-    # 当するものがない場合はnilを返す。
+    # た場合は、分割されたいずれかのうち、x座標、y座標を含む方を返す。
+    # 該当するものがない場合はnilを返す。
     def get_block(x, y, from=nil)
       from ||= @root_block
       if from.include?(x, y)
